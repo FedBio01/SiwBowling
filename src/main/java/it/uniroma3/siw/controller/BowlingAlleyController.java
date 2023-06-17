7cfdc74316c27738cf60cbfa9c67308d4eaedc16
FedBio01 <fed20ennerico@gmail.com>
2023-06-17 11:48:01 +0200

    refactor

--- src/main/java/it/uniroma3/siw/controller/BowlingAlleyController.java
+++ src/main/java/it/uniroma3/siw/controller/BowlingAlleyController.java
@@ -24,13 +24,13 @@ import jakarta.validation.Valid;
 @Controller
 public class BowlingAlleyController {
 	@Autowired 
-	private BowlingAlleyRepository movieRepository;
+	private BowlingAlleyRepository bowlingAlleyRepository;
 	
 	@Autowired 
-	private ReservationRepository artistRepository;
+	private ReservationRepository reservationRepository;
 
 	@Autowired 
-	private BowlingAlleyValidator movieValidator;
+	private BowlingAlleyValidator bowlingAlleyValidator;
 
 	@GetMapping(value="/admin/formNewMovie")
 	public String formNewMovie(Model model) {
@@ -40,7 +40,7 @@ public class BowlingAlleyController {
 
 	@GetMapping(value="/admin/formUpdateMovie/{id}")
 	public String formUpdateMovie(@PathVariable("id") Long id, Model model) {
-		model.addAttribute("movie", movieRepository.findById(id).get());
+		model.addAttribute("movie", bowlingAlleyRepository.findById(id).get());
 		return "admin/formUpdateMovie.html";
 	}
 
@@ -51,17 +51,17 @@ public class BowlingAlleyController {
 	
 	@GetMapping(value="/admin/manageMovies")
 	public String manageMovies(Model model) {
-		model.addAttribute("movies", this.movieRepository.findAll());
+		model.addAttribute("movies", this.bowlingAlleyRepository.findAll());
 		return "admin/manageMovies.html";
 	}
 	
 	@GetMapping(value="/admin/setDirectorToMovie/{directorId}/{movieId}")
 	public String setDirectorToMovie(@PathVariable("directorId") Long directorId, @PathVariable("movieId") Long movieId, Model model) {
 		
-		Reservation director = this.artistRepository.findById(directorId).get();
-		BowlingAlley movie = this.movieRepository.findById(movieId).get();
+		Reservation director = this.reservationRepository.findById(directorId).get();
+		BowlingAlley movie = this.bowlingAlleyRepository.findById(movieId).get();
 		movie.setDirector(director);
-		this.movieRepository.save(movie);
+		this.bowlingAlleyRepository.save(movie);
 		
 		model.addAttribute("movie", movie);
 		return "admin/formUpdateMovie.html";
@@ -70,17 +70,17 @@ public class BowlingAlleyController {
 	
 	@GetMapping(value="/admin/addDirector/{id}")
 	public String addDirector(@PathVariable("id") Long id, Model model) {
-		model.addAttribute("artists", artistRepository.findAll());
-		model.addAttribute("movie", movieRepository.findById(id).get());
+		model.addAttribute("artists", reservationRepository.findAll());
+		model.addAttribute("movie", bowlingAlleyRepository.findById(id).get());
 		return "admin/directorsToAdd.html";
 	}
 
 	@PostMapping("/admin/movie")
 	public String newMovie(@Valid @ModelAttribute("movie") BowlingAlley movie, BindingResult bindingResult, Model model) {
 		
-		this.movieValidator.validate(movie, bindingResult);
+		this.bowlingAlleyValidator.validate(movie, bindingResult);
 		if (!bindingResult.hasErrors()) {
-			this.movieRepository.save(movie); 
+			this.bowlingAlleyRepository.save(movie); 
 			model.addAttribute("movie", movie);
 			return "movie.html";
 		} else {
@@ -90,13 +90,13 @@ public class BowlingAlleyController {
 
 	@GetMapping("/movie/{id}")
 	public String getMovie(@PathVariable("id") Long id, Model model) {
-		model.addAttribute("movie", this.movieRepository.findById(id).get());
+		model.addAttribute("movie", this.bowlingAlleyRepository.findById(id).get());
 		return "movie.html";
 	}
 
 	@GetMapping("/movie")
 	public String getMovies(Model model) {		
-		model.addAttribute("movies", this.movieRepository.findAll());
+		model.addAttribute("movies", this.bowlingAlleyRepository.findAll());
 		return "movies.html";
 	}
 	
@@ -107,7 +107,7 @@ public class BowlingAlleyController {
 
 	@PostMapping("/searchMovies")
 	public String searchMovies(Model model, @RequestParam int year) {
-		model.addAttribute("movies", this.movieRepository.findByYear(year));
+		model.addAttribute("movies", this.bowlingAlleyRepository.findByYear(year));
 		return "foundMovies.html";
 	}
 	
@@ -116,18 +116,18 @@ public class BowlingAlleyController {
 
 		List<Reservation> actorsToAdd = this.actorsToAdd(id);
 		model.addAttribute("actorsToAdd", actorsToAdd);
-		model.addAttribute("movie", this.movieRepository.findById(id).get());
+		model.addAttribute("movie", this.bowlingAlleyRepository.findById(id).get());
 
 		return "admin/actorsToAdd.html";
 	}
 
 	@GetMapping(value="/admin/addActorToMovie/{actorId}/{movieId}")
 	public String addActorToMovie(@PathVariable("actorId") Long actorId, @PathVariable("movieId") Long movieId, Model model) {
-		BowlingAlley movie = this.movieRepository.findById(movieId).get();
-		Reservation actor = this.artistRepository.findById(actorId).get();
+		BowlingAlley movie = this.bowlingAlleyRepository.findById(movieId).get();
+		Reservation actor = this.reservationRepository.findById(actorId).get();
 		Set<Reservation> actors = movie.getActors();
 		actors.add(actor);
-		this.movieRepository.save(movie);
+		this.bowlingAlleyRepository.save(movie);
 		
 		List<Reservation> actorsToAdd = actorsToAdd(movieId);
 		
@@ -139,11 +139,11 @@ public class BowlingAlleyController {
 	
 	@GetMapping(value="/admin/removeActorFromMovie/{actorId}/{movieId}")
 	public String removeActorFromMovie(@PathVariable("actorId") Long actorId, @PathVariable("movieId") Long movieId, Model model) {
-		BowlingAlley movie = this.movieRepository.findById(movieId).get();
-		Reservation actor = this.artistRepository.findById(actorId).get();
+		BowlingAlley movie = this.bowlingAlleyRepository.findById(movieId).get();
+		Reservation actor = this.reservationRepository.findById(actorId).get();
 		Set<Reservation> actors = movie.getActors();
 		actors.remove(actor);
-		this.movieRepository.save(movie);
+		this.bowlingAlleyRepository.save(movie);
 
 		List<Reservation> actorsToAdd = actorsToAdd(movieId);
 		
@@ -156,7 +156,7 @@ public class BowlingAlleyController {
 	private List<Reservation> actorsToAdd(Long movieId) {
 		List<Reservation> actorsToAdd = new ArrayList<>();
 
-		for (Reservation a : artistRepository.findActorsNotInMovie(movieId)) {
+		for (Reservation a : reservationRepository.findActorsNotInMovie(movieId)) {
 			actorsToAdd.add(a);
 		}
 		return actorsToAdd;
